5b84ff4862f4da2ee15d3061aa7f48a13583371f
drewmercer-bl <drew.mercer@brandlive.com>
2024-10-15 16:12:34 -0700

    fix for serverless?

--- main.tf
+++ main.tf
@@ -186,7 +186,7 @@ resource "google_compute_backend_service" "default" {
   for_each = var.backends
 
   project = coalesce(each.value["project"], var.project)
-  name    = lookup(each.value, "name", "${var.name}-backend-${each.key}")
+  name    = lookup(each.value, "name")
 
   load_balancing_scheme = var.load_balancing_scheme
 
